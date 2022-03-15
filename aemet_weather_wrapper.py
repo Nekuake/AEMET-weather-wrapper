205fbceddff74fa52f10f9c196e8fce4c0c6614a
Nekuake <ruizale55@gmail.com>
2022-03-15 20:02:37 +0100

    Huge progress. Translating AEMET's JSON might be more complicated.

--- aemet_weather_wrapper.py
+++ aemet_weather_wrapper.py
@@ -1,51 +1,59 @@
 import requests
 import json
 import weather_config as cfg
-
+from datetime import datetime
 
 
 def config(new_api_key):
-	if (check_connection(new_api_key)):
-		cfg.api_key=new_api_key
+    if check_connection(new_api_key):
+        cfg.api_key = new_api_key
 
-def config():
-	new_api_key=input("Paste API Key. You can get it from https://opendata.aemet.es/centrodedescargas/altaUsuario>>")
-	if (check_connection(new_api_key)):
-		cfg.api_key=new_api_key
 
 def check_connection(api_key):
-	if api_key == "":
-		return False #We would just check if server returns a 200 or a 401 but as for 15/03/22 if the api_key is blank, it will just return an empty 200.
-	else:
-		r = requests.get('https://opendata.aemet.es/opendata/api/maestro/municipios', headers={"api_key":api_key})
-		if r.status_code == 200:
-			print ("API Key and connection OK")
-			return True
-		else:
-			print ("Request returned "+ str(r.status_code))
-			return False
+    if api_key == "":
+        raise Exception("API Key is blank. Check 'weather_config.py.")
+    else:
+        r = requests.get('https://opendata.aemet.es/opendata/api/maestro/municipios', headers={"api_key": api_key})
+        if r.status_code == 200:
+            print("API Key and connection OK")
+            return True
+        else:
+            print("Request returned " + str(r.status_code))
+            raise Exception("Request returned" + str(r.status_code))
 
-def external_help():  #Downloads and shows AEMET's OpenData JSON
-	r=requests.get("https://opendata.aemet.es/AEMET_OpenData_specification.json")
-	
 
-def get_Forecast_Daily(city_code):  #It's better to use this way to get the forecast.
-	r=requests.get("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/" + str(city_code), headers={"api_key": cfg.api_key})
-	DataJsonURL=json.loads(r.text)
-	DataJsonURL=DataJsonURL.get["datos"]
-	print (r.text)
+def external_help():  # Downloads and shows AEMET's OpenData JSON. No API Key needed.
+    r = requests.get("https://opendata.aemet.es/AEMET_OpenData_specification.json")
+    print(r.text)
+
+
+def get_forecast_daily_from_code(city_code):  # It's better to use this way to get the forecast.
+    r = requests.get("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/" + str(city_code),
+                     headers={"api_key": cfg.api_key})
+    data_json_url = json.loads(r.text)
+    response_dict = json.loads(requests.get(data_json_url.get("datos")).text)
+    update_time = datetime.strptime(response_dict[0].get("elaborado"), "%Y-%m-%dT%H:%M:%S")
+    sections = {}
+    # Here I thought it would be better to have a more graphical approach, avoiding for loops and showing how the
+    # AEMET API delivers the data. It may look clunkier and I'm open to change it.
+    for x in len(response_dict[0].get("prediccion").get("dia")):
+        for x in len(response_dict[0].get("prediccion").get("dia"):
+        	pass
+    new_forecast = Forecast(response_dict.get("nombre"), city_code, True, update_time, sections)
+    return new_forecast
 
 
 class Forecast:
-	def __init__(self,city_name, city_code, days):
-		self.city_name=city_name
-		self.city_code=city_code
-		self.days=days
+    def __init__(self, city_name, city_code, is_daily, update_time, sections):
+        self.city_name = city_name
+        self.city_code = city_code
+        self.is_daily = is_daily
+        self.update_time = update_time
+        self.sections = sections
 
-	def __init__(self, city_code):
-		self.city_code=city_code
-	@classmethod
-	def refresh_forecast(self):
-		pass
+    @classmethod
+    def refresh_forecast(self):
+        pass
 
 
+get_forecast_daily_from_code(26011)
