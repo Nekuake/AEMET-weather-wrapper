e9790c17448c90bc5a46ae17c0cfc4b7ff1f5202
nekuake <ruizale55@gmail.com>
2022-03-15 11:54:14 +0100

    Little test of checking the API key

--- aemet-weather-wrapper.py
+++ aemet-weather-wrapper.py
@@ -2,14 +2,31 @@ import requests
 import pprint
 import weather_config as cfg
 
-global api_key=null
 
 
 def config(new_api_key):
-	api_key=new_api_key
+	if (check_connection(new_api_key)):
+		weather_config.api_key=new_api_key
+
+def config():
+	new_api_key=input("Paste API Key. You can get it from https://opendata.aemet.es/centrodedescargas/altaUsuario>>")
+	if (check_connection(new_api_key)):
+		weather_config.api_key=new_api_key
+
+def check_connection(api_key):
+	if api_key == "":
+		return false #We would just check if server returns a 200 or a 401 but as for 15/03/22 if the api_key is blank, it will just return an empty 200.
+	else:
+		r = requests.get('https://opendata.aemet.es/opendata/api/maestro/municipios', headers={"api_key":api_key)
+		if r.status_code == "200":
+			print ("API Key and connection OK")
+			return true
+		else:
+			print ("Request returned "+ r.status_code)
+			return false
 
 def external_help():  #Downloads and shows AEMET's OpenData JSON
-	r=requests.get(https://opendata.aemet.es/AEMET_OpenData_specification.json)
+	r=requests.get("https://opendata.aemet.es/AEMET_OpenData_specification.json", headers={"api_key":api_key})
 	
 
 
