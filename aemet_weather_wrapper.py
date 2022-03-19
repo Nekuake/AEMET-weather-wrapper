e03b9f92ada5f915d94409d2fc652a97b1d0636c
Nekuake <ruizale55@gmail.com>
2022-03-19 13:45:42 +0100

    Get Forecast and Code working.

--- aemet_weather_wrapper.py
+++ aemet_weather_wrapper.py
@@ -18,7 +18,6 @@ def check_connection(api_key):
             print("API Key and connection OK")
             return True
         else:
-            print("Request returned " + str(r.status_code))
             raise Exception("Request returned" + str(r.status_code))
 
 
@@ -36,26 +35,57 @@ def get_forecast_daily_from_code(city_code):  # It's better to use this way to g
     update_time = datetime.strptime(response_dict[0]["elaborado"], "%Y-%m-%dT%H:%M:%S")
     sections = {}
     # Here I thought it would be better to have a more graphical approach, avoiding for loops and showing how the
-    # AEMET API delivers the data. It may look clunkier and I'm open to change it.
+    # AEMET API delivers the data. It may look clunkier, so I'm open to change it.
     for day in (response_dict[0]["prediccion"]["dia"]):
-        aux_date=day["fecha"].removesuffix("T00:00:00")
-        
-        sections[aux_date]={}
+        aux_date = day["fecha"].removesuffix("T00:00:00")
+
+        sections[aux_date] = {}
         for period in (day["probPrecipitacion"]):
-            sections[aux_date][period["periodo"]]={}
-            sections[aux_date][period["periodo"]]["rainfall_probability"]=period["value"]
+            try:
+                sections[aux_date][period["periodo"]] = {}
+                sections[aux_date][period["periodo"]]["rainfall_probability"] = period["value"]
+            except KeyError:
+                # AEMET will not populate any timestamps after Day 4, just the value, so we'll implement it. This is
+                # not the most beautiful way but I am focused in dumping the whole forecast.
+                sections[aux_date]["00-24"] = {}
+                sections[aux_date]["00-24"]["rainfall_probability"] = period["value"]
         for period in (day["estadoCielo"]):
-            sections[aux_date][period["periodo"]]["weather_code"]=cfg.status_weather_codes[period["value"][0:2]]
+            try:
+                sections[aux_date][period["periodo"]]["weather_code"] = cfg.status_weather_codes[period["value"][0:2]]
+            except KeyError:
+                sections[aux_date]["00-24"]["weather_code"] = cfg.status_weather_codes[period["value"][0:2]]
         for period in (day["cotaNieveProv"]):
-            sections[aux_date][period["periodo"]]["snow_level"]=period["value"]
+            try:
+                sections[aux_date][period["periodo"]]["snow_level"] = period["value"]
+            except KeyError:
+                sections[aux_date]["00-24"]["snow_level"] = period["value"]
         for period in (day["rachaMax"]):
-            sections[aux_date][period["periodo"]]["wind_gusts"]=period["value"]
+            try:
+                sections[aux_date][period["periodo"]]["wind_gusts"] = period["value"]
+            except KeyError:
+                sections[aux_date]["00-24"]["wind_gusts"] = period["value"]
         for period in (day["viento"]):
-            sections[aux_date][period["periodo"]]["wind_gusts"]= {"direction": period["direccion"],
-                                                                  "speed": period["velocidad"]}
-    new_forecast = Forecast(response_dict["nombre"], city_code, True, update_time, sections)
+            try:
+                sections[aux_date][period["periodo"]]["wind_gusts"] = {"direction": period["direccion"],
+                                                                    "speed": period["velocidad"]}
+            except KeyError:
+                sections[aux_date]["00-24"]["wind_gusts"] = {"direction": period["direccion"],
+                                                                       "speed": period["velocidad"]}
+    new_forecast = Forecast(response_dict[0]["nombre"], city_code, True, update_time, sections)
     return new_forecast
 
+
+def get_code_from_name(city_name):  #This is not the ideal way to get the forecast. I would recommend getting it by code
+    response_dict=json.loads(requests.get("https://opendata.aemet.es/opendata/api/maestro/municipios", headers={"api_key" : cfg.api_key}).text)
+    search = list(filter(lambda city: city['nombre'] == city_name, response_dict))
+    if(len(search)>1):
+        raise Warning("More than one match. Returning all matches as list.")
+    matches = {}
+    for results in search:
+        matches[results["nombre"]] = {}
+        matches[results["nombre"]]=results["id"].removeprefix("id")  #This was introduced in Python 3.9
+    print(matches)
+    return matches
 
 class Forecast:
     def __init__(self, city_name, city_code, is_daily, update_time, sections):
@@ -67,7 +97,9 @@ class Forecast:
 
     @classmethod
     def refresh_forecast(self):
-        pass
+        get_forecast_daily_from_code(self.city_code)
 
 
-get_forecast_daily_from_code(26011)
+get_forecast_daily_from_code(get_code_from_name("Alfaro")["Alfaro"])
+
+
